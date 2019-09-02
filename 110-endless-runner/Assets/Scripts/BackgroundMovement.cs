33316e361179f73a35af7d5635351707f8167c91
bdadam_enok <brandon.dadam@enok.co>
2019-09-02 17:34:17 -0500

    Moved some files around and setup percentage based background display behavior.

--- 110-endless-runner/Assets/Scripts/BackgroundMovement.cs
+++ 110-endless-runner/Assets/Scripts/BackgroundMovement.cs
@@ -7,6 +7,12 @@ public class BackgroundMovement : MonoBehaviour
     public float backgroundSpeed = -5f;
     public GameObject otherBackground;
 
+    public int showBackground1 = 25;
+    public int showBackground2 = 25;
+    public int showBackground3 = 25;
+    public int showBackground4 = 25;
+
+
     void Update()
     {
         transform.position += new Vector3(backgroundSpeed * Time.deltaTime, 0, 0);
@@ -16,6 +22,8 @@ public class BackgroundMovement : MonoBehaviour
     {
         if (transform.position.x <= -22)
         {
+            ChangeBackgroundArt();
+
             float halfOtherBackgroundWidth = otherBackground.GetComponent<SpriteRenderer>().bounds.extents.x;
             float halfThisBackgroundWidth = GetComponent<SpriteRenderer>().bounds.extents.x;
 
@@ -23,6 +31,28 @@ public class BackgroundMovement : MonoBehaviour
             newXPosition -= .25f;
 
             transform.position = new Vector3(newXPosition, otherBackground.transform.position.y);
+        }
+    }
+
+    void ChangeBackgroundArt()
+    {
+        int randomNumber = Random.Range(0, 100);
+
+        if (randomNumber < showBackground1)
+        {
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/tile1");
+        }
+        else  if (randomNumber < showBackground2 + showBackground1)
+        {
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/tile2");
+        }
+        else if (randomNumber < showBackground3 + showBackground2 + showBackground1)
+        {
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/tile3");
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprites/tile4");
         }
     }
 }
