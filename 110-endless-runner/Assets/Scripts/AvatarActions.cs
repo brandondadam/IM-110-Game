11cb8ecf1485080d953d123acdc05316dbd853a5
bdadam_enok <brandon.dadam@enok.co>
2019-09-11 01:24:32 -0500

    Added an avatar and ground for jumping. Handles double jumping as well.

---
+++ 110-endless-runner/Assets/Scripts/AvatarActions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarActions : MonoBehaviour
+{
+    bool canJump = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.J))
+        {
+            if (canJump)
+            {
+                canJump = false;
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 17), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        canJump |= collision.gameObject.tag == "Ground";
+    }
+}
